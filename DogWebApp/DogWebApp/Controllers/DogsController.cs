97493722a0279f49baf9db46b23848eada2f638b
martushoka <martushoka.10@gmail.com>
2023-01-25 09:44:52 +0200

    Dogs

---
+++ DogWebApp/DogWebApp/Controllers/DogsController.cs
@@ -0,0 +1,26 @@
+﻿using DogWebApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogWebApp.Controllers
+{
+    public class DogsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public DogsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+    }
+    
+}
