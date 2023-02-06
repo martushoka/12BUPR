2870eb97f79a21044592aad3dccb2414a1a5fcc9
martushoka <martushoka.10@gmail.com>
2023-02-06 13:14:47 +0200

    dogs

---
+++ DogWebApp/Controllers/DogsController.cs
@@ -0,0 +1,50 @@
+﻿using DogWebApp.Data;
+using DogWebApp.Domain;
+using DogWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogWebApp.Controllers
+{
+    public class DogsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public DogsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+
+        public IActionResult Create(DogCreateViewModel bindingModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Dog dogFromDb = new Dog
+                {
+                    Name = bindingModel.Name,
+                    Age = bindingModel.Age,
+                    Breed = bindingModel.Breed,
+                    Picture = bindingModel.Picture,
+
+
+                };
+
+                context.Dogs.Add(dogFromDb);
+                context.SaveChanges();
+
+                return this.RedirectToAction("Success");
+            }
+            return this.View();
+        }
+
+        public IActionResult Success()
+        {
+            return this.View();
+        }
+    }
+    
+}
---
+++ DogWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DogWebApp.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DogWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Dog> Dogs { get; set; }
+    }
+}
